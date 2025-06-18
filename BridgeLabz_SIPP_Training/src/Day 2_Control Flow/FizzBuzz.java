2cb51cfef5b53676c0027f5fd4c17c08dee84c62
kartikey <kartkp@example.com>
2025-06-18 09:29:25 +0530

    Kartikey Add FizzBuzz

--- BridgeLabz_SIPP_Training/src/Day 2_Control Flow/FizzBuzz.java
+++ BridgeLabz_SIPP_Training/src/Day 2_Control Flow/FizzBuzz.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class FizzBuzz {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), i = 1;
         if (n > 0) {
