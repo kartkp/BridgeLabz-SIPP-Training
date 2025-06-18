2771dc16a62b74577a43cbed01d6eac4251113af
kartikey <kartkp@example.com>
2025-06-18 10:16:15 +0530

    Kartikey Add FizzBuzz

--- src/Day3_ArrayPractice/FizzBuzz.java
+++ src/Day3_ArrayPractice/FizzBuzz.java
@@ -1,7 +1,6 @@
 package Day3_ArrayPractice;
 
 public class FizzBuzz {
-
     public static void main(String[] args) {
         java.util.Scanner sc = new java.util.Scanner(System.in);
         System.out.print("Enter a positive number: ");
