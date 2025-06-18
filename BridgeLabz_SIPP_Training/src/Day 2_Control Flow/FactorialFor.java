9a2af2f5d5602351b87ca5bbdac779079ba6ded7
kartikey <kartkp@example.com>
2025-06-18 09:27:09 +0530

    Kartikey Add FactorialFor

--- BridgeLabz_SIPP_Training/src/Day 2_Control Flow/FactorialFor.java
+++ BridgeLabz_SIPP_Training/src/Day 2_Control Flow/FactorialFor.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class FactorialFor {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), fact = 1;
         if (n >= 0) {
