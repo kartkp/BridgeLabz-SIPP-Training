2cd31c97f7f69e0478778e9b41d5315ea8ac8030
kartikey <kartkp@example.com>
2025-06-18 09:33:44 +0530

    Kartikey Add FriendsComparison

--- BridgeLabz_SIPP_Training/src/Day 2_Control Flow/FriendsComparison.java
+++ BridgeLabz_SIPP_Training/src/Day 2_Control Flow/FriendsComparison.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 public class FriendsComparison {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int age1 = sc.nextInt(), height1 = sc.nextInt(); // Amar
-        int age2 = sc.nextInt(), height2 = sc.nextInt(); // Akbar
-        int age3 = sc.nextInt(), height3 = sc.nextInt(); // Anthony
+        
+        int age1 = sc.nextInt(), height1 = sc.nextInt();
+        int age2 = sc.nextInt(), height2 = sc.nextInt();
+        int age3 = sc.nextInt(), height3 = sc.nextInt();
 
         int youngestAge = Math.min(age1, Math.min(age2, age3));
         int tallestHeight = Math.max(height1, Math.max(height2, height3));
