0cf2b96dcac446315859c1d2143648c98341d0d8
kartikey <kartkp@example.com>
2025-06-18 09:38:22 +0530

    Kartikey Add LargestCheck

--- BridgeLabz_SIPP_Training/src/Day 2_Control Flow/LargestCheck.java
+++ BridgeLabz_SIPP_Training/src/Day 2_Control Flow/LargestCheck.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class LargestCheck {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
         System.out.println("Is the first number the largest? " + (a > b && a > c));
