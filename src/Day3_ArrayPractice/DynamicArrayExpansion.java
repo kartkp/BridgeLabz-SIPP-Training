5f04f86140bafe62b1d947c6eb6061bf994a62a6
kartikey <kartkp@example.com>
2025-06-18 10:15:40 +0530

    Kartikey Add DynamicArrayExpansion

--- src/Day3_ArrayPractice/DynamicArrayExpansion.java
+++ src/Day3_ArrayPractice/DynamicArrayExpansion.java
@@ -1,6 +1,7 @@
 package Day3_ArrayPractice;
 
 public class DynamicArrayExpansion {
+    
     public static void main(String[] args) {
         java.util.Scanner sc = new java.util.Scanner(System.in);
         System.out.print("Enter a number to find its factors: ");
