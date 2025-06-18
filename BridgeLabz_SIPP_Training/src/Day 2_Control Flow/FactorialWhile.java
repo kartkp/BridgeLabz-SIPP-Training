0e7cf3de4881b3d071623f035cf2da9be4174860
kartikey <kartkp@example.com>
2025-06-18 09:27:47 +0530

    Kartikey Add FactorialWhile

--- BridgeLabz_SIPP_Training/src/Day 2_Control Flow/FactorialWhile.java
+++ BridgeLabz_SIPP_Training/src/Day 2_Control Flow/FactorialWhile.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class FactorialWhile {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         if (n >= 0) {
