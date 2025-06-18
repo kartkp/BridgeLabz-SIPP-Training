c118cae9ec7c3fe169ad4f16373ed60d3c48c806
kartikey <kartkp@example.com>
2025-06-18 10:11:20 +0530

    Kartikey Add BMIChecker

--- src/Day 2_Control Flow/BMIChecker.java
+++ src/Day 2_Control Flow/BMIChecker.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class BMIChecker {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         double weight = sc.nextDouble();
         double heightCm = sc.nextDouble();
