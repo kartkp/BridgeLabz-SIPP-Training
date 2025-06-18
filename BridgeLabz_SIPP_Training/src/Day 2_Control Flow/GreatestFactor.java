9e69e41c7e39ed61692e1c64096d9fe1696f90c8
kartikey <kartkp@example.com>
2025-06-18 09:36:24 +0530

    Kartikey Add GreatestFactor

--- BridgeLabz_SIPP_Training/src/Day 2_Control Flow/GreatestFactor.java
+++ BridgeLabz_SIPP_Training/src/Day 2_Control Flow/GreatestFactor.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class GreatestFactor {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         int greatestFactor = 1;
