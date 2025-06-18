0e7409ef6ab6f1122c9dcaea8064ca7d313b505f
kartikey <kartkp@example.com>
2025-06-18 09:17:34 +0530

    Kartikey Add EmployeeBonus

--- BridgeLabz_SIPP_Training/src/Day 2_Control Flow/EmployeeBonus.java
+++ BridgeLabz_SIPP_Training/src/Day 2_Control Flow/EmployeeBonus.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class EmployeeBonus {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         double salary = sc.nextDouble();
         int years = sc.nextInt();
         if (years > 5) {
