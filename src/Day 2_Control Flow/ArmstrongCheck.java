b2ba7a0cfa47bc546e24fb621ff9ab79572f42c5
kartikey <kartkp@example.com>
2025-06-18 10:05:34 +0530

    Kartikey Add ArmstrongCheck

--- src/Day 2_Control Flow/ArmstrongCheck.java
+++ src/Day 2_Control Flow/ArmstrongCheck.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class ArmstrongCheck {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         int sum = 0, original = number;
