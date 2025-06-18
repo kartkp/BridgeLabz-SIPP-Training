bdd14a6a5fdca46b245b2f825add85505ceaf3a9
kartikey <kartkp@example.com>
2025-06-18 09:16:58 +0530

    Kartikey Add DivisibleBy5

--- BridgeLabz_SIPP_Training/src/Day 2_Control Flow/DivisibleBy5.java
+++ BridgeLabz_SIPP_Training/src/Day 2_Control Flow/DivisibleBy5.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class DivisibleBy5 {
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
