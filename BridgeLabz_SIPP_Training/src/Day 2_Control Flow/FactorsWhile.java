0d5cdfb2c216e1fca9cbea4cf5d6958683956d73
kartikey <kartkp@example.com>
2025-06-18 09:28:48 +0530

    Kartikey Add FactorsWhile

--- BridgeLabz_SIPP_Training/src/Day 2_Control Flow/FactorsWhile.java
+++ BridgeLabz_SIPP_Training/src/Day 2_Control Flow/FactorsWhile.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class FactorsWhile {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt(), counter = 1;
         if (number > 0) {
