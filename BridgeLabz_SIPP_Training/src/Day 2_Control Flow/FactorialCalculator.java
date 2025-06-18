e6f57f275daf349b987c87bfcefa6d7ab5a044ad
kartikey <kartkp@example.com>
2025-06-18 09:21:37 +0530

    Kartikey Add FactorialCalculator

--- BridgeLabz_SIPP_Training/src/Day 2_Control Flow/FactorialCalculator.java
+++ BridgeLabz_SIPP_Training/src/Day 2_Control Flow/FactorialCalculator.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class FactorialCalculator {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         
         System.out.print("Enter a positive integer: ");
