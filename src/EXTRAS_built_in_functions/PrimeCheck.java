3922b3625b61c00c8a2ac482d7cfb57f66d8f50a
kartikey <p.kartikey.p@gmail.com>
2025-06-24 11:56:25 +0530

    Kartikey Add PrimeCheck

---
+++ src/EXTRAS_built_in_functions/PrimeCheck.java
@@ -0,0 +1,19 @@
+package EXTRAS_built_in_functions;
+
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        System.out.println(num + (isPrime(num) ? " is a prime number." : " is not a prime number."));
+    }
+
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++)
+            if (n % i == 0) return false;
+        return true;
+    }
+}
