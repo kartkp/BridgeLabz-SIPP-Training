84c1faf110b9bd1d8470e79ce596f95f95747b6e
kartikey <p.kartikey.p@gmail.com>
2025-06-24 11:59:31 +0530

    Kartikey Add Fibonacci

---
+++ src/EXTRAS_built_in_functions/Fibonacci.java
@@ -0,0 +1,23 @@
+package EXTRAS_built_in_functions;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of terms: ");
+        int n = scanner.nextInt();
+        generateFibonacci(n);
+    }
+
+    static void generateFibonacci(int n) {
+        int a = 0, b = 1;
+        System.out.print("Fibonacci sequence: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
