08061f41281be89d5ef504aa823ea6372f3f6a74
kartikey <p.kartikey.p@gmail.com>
2025-06-25 12:08:39 +0530

    Kartikey Add PalindromeChecker

--- Java_Class_and_Object/Level2/PalindromeChecker.java
+++ Java_Class_and_Object/Level2/PalindromeChecker.java
@@ -1,5 +1,29 @@
 package Java_Class_and_Object.Level2;
 
 public class PalindromeChecker {
-    
+    private String text;
+
+    public PalindromeChecker(String text) {
+        this.text = text;
+    }
+
+    public boolean isPalindrome() {
+        String cleaned = text.replaceAll("\\s+", "").toLowerCase();
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+        return cleaned.equals(reversed);
+    }
+
+    public void displayResult() {
+        System.out.println("Text: " + text);
+        if (isPalindrome()) {
+            System.out.println("Result: It is a palindrome.");
+        } else {
+            System.out.println("Result: It is not a palindrome.");
+        }
+    }
+
+    public static void main(String[] args) {
+        PalindromeChecker checker = new PalindromeChecker("Madam");
+        checker.displayResult();
+    }
 }
