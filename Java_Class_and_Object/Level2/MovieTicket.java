82fd611b8416bfa249dcd253caf4e406970d0e99
kartikey <p.kartikey.p@gmail.com>
2025-06-25 12:09:00 +0530

    Kartikey Add MovieTicket

--- Java_Class_and_Object/Level2/MovieTicket.java
+++ Java_Class_and_Object/Level2/MovieTicket.java
@@ -1,5 +1,26 @@
 package Java_Class_and_Object.Level2;
 
 public class MovieTicket {
-    
+    private String movieName;
+    private int seatNumber;
+    private double price;
+
+    public void bookTicket(String movieName, int seatNumber, double price) {
+        this.movieName = movieName;
+        this.seatNumber = seatNumber;
+        this.price = price;
+        System.out.println("Ticket booked successfully!");
+    }
+
+    public void displayDetails() {
+        System.out.println("Movie Name: " + movieName);
+        System.out.println("Seat Number: " + seatNumber);
+        System.out.println("Price: ₹" + price);
+    }
+
+    public static void main(String[] args) {
+        MovieTicket ticket = new MovieTicket();
+        ticket.bookTicket("RRR 2", 15, 250.00);
+        ticket.displayDetails();
+    }
 }
