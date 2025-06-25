ff0067e46cd6ce880a56ca27e551d3e30c9f0c77
kartikey <p.kartikey.p@gmail.com>
2025-06-25 12:05:27 +0530

    Kartikey Add Circle

--- Java_Class_and_Object/Level1/Circle.java
+++ Java_Class_and_Object/Level1/Circle.java
@@ -1,5 +1,28 @@
 package Java_Class_and_Object.Level1;
 
 public class Circle {
-    
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double calculateCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public void displayDetails() {
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + calculateArea());
+        System.out.println("Circumference: " + calculateCircumference());
+    }
+
+    public static void main(String[] args) {
+        Circle c = new Circle(5.5);
+        c.displayDetails();
+    }
 }
