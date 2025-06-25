fe6d084b26d8b073defbcdd3ba379e135bd79441
kartikey <p.kartikey.p@gmail.com>
2025-06-25 12:07:09 +0530

    Kartikey Add MobilePhone

--- Java_Class_and_Object/Level1/MobilePhone.java
+++ Java_Class_and_Object/Level1/MobilePhone.java
@@ -1,5 +1,24 @@
 package Java_Class_and_Object.Level1;
 
 public class MobilePhone {
-    
+    private String brand;
+    private String model;
+    private double price;
+
+    public MobilePhone(String brand, String model, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Price: ₹" + price);
+    }
+
+    public static void main(String[] args) {
+        MobilePhone phone = new MobilePhone("Samsung", "Galaxy S24", 74999.99);
+        phone.displayDetails();
+    }
 }
