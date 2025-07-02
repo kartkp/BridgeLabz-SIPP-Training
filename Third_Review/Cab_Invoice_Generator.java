f92b97f19d9f905a231d2d49ead2e9d5f262c054
kartikey <p.kartikey.p@gmail.com>
2025-07-02 12:32:55 +0530

    Kartikey Add Cab_Invoice_Generator

--- Third_Review/Cab_Invoice_Generator.java
+++ Third_Review/Cab_Invoice_Generator.java
@@ -3,6 +3,7 @@ public class Cab_Invoice_Generator {
     public static void main(String[] args) {
         InvoiceService invoiceService = new InvoiceService();
 
+        
         Ride[] rides = {
             new Ride(2.0, 5, RideCategory.NORMAL),
             new Ride(0.1, 1, RideCategory.NORMAL),
