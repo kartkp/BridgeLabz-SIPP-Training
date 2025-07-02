776a6c85b2e9377ded95b60b501045f83907a772
kartikey <p.kartikey.p@gmail.com>
2025-07-02 12:30:57 +0530

    Kartikey Add InvoiceSummary

--- Third_Review/InvoiceSummary.java
+++ Third_Review/InvoiceSummary.java
@@ -3,7 +3,6 @@ public class InvoiceSummary {
     public int totalRides;
     public double totalFare;
     public double averageFare;
-
     public InvoiceSummary(int totalRides, double totalFare) {
         this.totalRides = totalRides;
         this.totalFare = totalFare;
