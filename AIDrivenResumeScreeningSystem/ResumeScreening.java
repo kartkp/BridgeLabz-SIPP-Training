7db85fda4d94f966550dda43382d7ad6d56f6e10
kartikey <p.kartikey.p@gmail.com>
2025-07-22 10:19:36 +0530

    Kartikey Add ResumeProcessor

---
+++ AIDrivenResumeScreeningSystem/ResumeScreening.java
@@ -0,0 +1,15 @@
+package AIDrivenResumeScreeningSystem;
+
+import java.util.*;
+public class ResumeScreening {
+    public static void main(String[] args) {
+        List<Resume<? extends JobRole>> resumeList = new ArrayList<>();
+
+        resumeList.add(new Resume<>("Amit", new SoftwareEngineer()));
+        resumeList.add(new Resume<>("Balram", new DataScientist()));
+        resumeList.add(new Resume<>("Chaman", new ProductManager()));
+
+        System.out.println("AI-Driven Resume Screening:\n");
+        ResumeProcessor.processResumes(resumeList);
+    }
+}
