188c3284f80ec0dafb1be7c12bfcb666512553f3
kartikey <p.kartikey.p@gmail.com>
2025-07-22 10:03:33 +0530

    Kartikey Add mainMealPlanGenerator

---
+++ PersonalizedMealPlanGenerator/mainMealPlanGenerator.java
@@ -0,0 +1,17 @@
+package PersonalizedMealPlanGenerator;
+
+public class mainMealPlanGenerator {
+    public static void main(String[] args) {
+
+        Meal<VegetarianMeal> vegMeal = new Meal<>(new VegetarianMeal());
+        Meal<VeganMeal> veganMeal = new Meal<>(new VeganMeal());
+        Meal<KetoMeal> ketoMeal = new Meal<>(new KetoMeal());
+
+        System.out.println("Meal Plan Generator:\n");
+
+        MealPlanGenerator.generatePlan(vegMeal.getPlan());
+        MealPlanGenerator.generatePlan(veganMeal.getPlan());
+        MealPlanGenerator.generatePlan(ketoMeal.getPlan());
+    }
+}
+
