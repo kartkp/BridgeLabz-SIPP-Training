9a2493dd9e54d594b72a6e5938bf44bc0542cc7e
kartikey <p.kartikey.p@gmail.com>
2025-07-22 09:30:04 +0530

    Kartikey Add Category

---
+++ DynamicOnlineMarketplace/Category.java
@@ -0,0 +1,8 @@
+package DynamicOnlineMarketplace;
+
+class Category {
+    private String categoryName;
+    public Category(String categoryName) { this.categoryName = categoryName; }
+    public String getCategoryName() { return categoryName; }
+}
+
