96fba8cca9b083eaf1fad2f3e654520778053f94
kartikey <p.kartikey.p@gmail.com>
2025-07-22 09:33:30 +0530

    Kartikey Add CourseType

---
+++ MultiLevelUniversityCourseManagementSystem/CourseType.java
@@ -0,0 +1,11 @@
+package MultiLevelUniversityCourseManagementSystem;
+
+abstract class CourseType {
+    private String title;
+
+    public CourseType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() { return title; }
+}
