ba590eeb85b0af8051e6e717901ded2142d48529
kartikey <p.kartikey.p@gmail.com>
2025-07-22 10:17:17 +0530

    Kartikey Add Resume

---
+++ AIDrivenResumeScreeningSystem/Resume.java
@@ -0,0 +1,14 @@
+package AIDrivenResumeScreeningSystem;
+
+class Resume<T extends JobRole> {
+    private String candidateName;
+    private T jobRole;
+
+    public Resume(String candidateName, T jobRole) {
+        this.candidateName = candidateName;
+        this.jobRole = jobRole;
+    }
+
+    public String getCandidateName() { return candidateName; }
+    public T getJobRole() { return jobRole; }
+}
