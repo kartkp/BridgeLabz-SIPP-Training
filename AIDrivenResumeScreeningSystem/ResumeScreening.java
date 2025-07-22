a1f969e6f605bf519ce68fa0c2a4979d28d7c19f
kartikey <p.kartikey.p@gmail.com>
2025-07-22 10:24:27 +0530

    Kartikey Add ResumeScreening

--- AIDrivenResumeScreeningSystem/ResumeScreening.java
+++ AIDrivenResumeScreeningSystem/ResumeScreening.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class ResumeScreening {
     public static void main(String[] args) {
         List<Resume<? extends JobRole>> resumeList = new ArrayList<>();
-
         resumeList.add(new Resume<>("Amit", new SoftwareEngineer()));
         resumeList.add(new Resume<>("Balram", new DataScientist()));
         resumeList.add(new Resume<>("Chaman", new ProductManager()));
