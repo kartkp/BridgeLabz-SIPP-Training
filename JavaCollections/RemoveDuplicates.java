087f5171287dd31bbb14e4f6c3825ee14a27f921
kartikey <p.kartikey.p@gmail.com>
2025-07-25 09:08:51 +0530

    Kartikey Add RemoveDuplicates

---
+++ JavaCollections/RemoveDuplicates.java
@@ -0,0 +1,21 @@
+package JavaCollections;
+import java.util.*;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(3, 1, 2, 2, 3, 4);
+
+        Set<Integer> seen = new HashSet<>();
+        List<Integer> result = new ArrayList<>();
+
+        for (Integer num : list) {
+            if (!seen.contains(num)) {
+                seen.add(num);
+                result.add(num);
+            }
+        }
+
+        System.out.println(result);
+    }
+}
+
