7846a34078bcef20640b5977436442b8ea0519dd
kartikey <p.kartikey.p@gmail.com>
2025-07-25 09:08:07 +0530

    Kartikey Add RotateList

---
+++ JavaCollections/RotateList.java
@@ -0,0 +1,23 @@
+package JavaCollections;
+
+import java.util.*;
+
+public class RotateList {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(10, 20, 30, 40, 50));
+        int rotateBy = 2;
+        int n = list.size();
+
+        List<Integer> rotated = new ArrayList<>();
+
+        for (int i = rotateBy; i < n; i++) {
+            rotated.add(list.get(i));
+        }
+        for (int i = 0; i < rotateBy; i++) {
+            rotated.add(list.get(i));
+        }
+
+        System.out.println(rotated);
+    }
+}
+
