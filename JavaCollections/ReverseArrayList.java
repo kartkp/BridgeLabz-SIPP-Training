ea295a36b8ff681b75c84fcd118e459e73aca8e4
kartikey <p.kartikey.p@gmail.com>
2025-07-25 08:59:10 +0530

    Kartikey Add ReverseArrayList

---
+++ JavaCollections/ReverseArrayList.java
@@ -0,0 +1,19 @@
+package JavaCollections;
+
+import java.util.*;
+
+public class ReverseArrayList {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+
+        int left = 0, right = list.size() - 1;
+        while (left < right) {
+            int temp = list.get(left);
+            list.set(left, list.get(right));
+            list.set(right, temp);
+            left++;
+            right--;
+        }
+        System.out.println(list);
+    }
+}
