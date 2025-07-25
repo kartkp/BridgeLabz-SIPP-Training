f8337445a83fb27cb33984bb555aa8cebce38874
kartikey <p.kartikey.p@gmail.com>
2025-07-25 09:49:12 +0530

    Kartikey Add Employee

---
+++ JavaCollections/MapInterface/Employee.java
@@ -0,0 +1,16 @@
+package JavaCollections.MapInterface;
+
+public class Employee {
+    String name;
+    String department;
+
+    public Employee(String name, String department) {
+        this.name = name;
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
