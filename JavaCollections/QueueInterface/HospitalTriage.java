5c4d2efabff9a127e1d10af7226ea402c12699b8
kartikey <p.kartikey.p@gmail.com>
2025-07-25 09:42:29 +0530

    Kartikey Add Patient

---
+++ JavaCollections/QueueInterface/HospitalTriage.java
@@ -0,0 +1,36 @@
+package JavaCollections.QueueInterface;
+
+import java.util.*;
+
+class Patient implements Comparable<Patient> {
+    String name;
+    int severity;
+
+    public Patient(String name, int severity) {
+        this.name = name;
+        this.severity = severity;
+    }
+
+    @Override
+    public int compareTo(Patient other) {
+        return Integer.compare(other.severity, this.severity);
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + severity + ")";
+    }
+}
+
+public class HospitalTriage {
+    public static void main(String[] args) {
+        PriorityQueue<Patient> pq = new PriorityQueue<>();
+        pq.add(new Patient("John", 3));
+        pq.add(new Patient("Alice", 5));
+        pq.add(new Patient("Bob", 2));
+
+        while (!pq.isEmpty()) {
+            System.out.println(pq.poll());
+        }
+    }
+}
