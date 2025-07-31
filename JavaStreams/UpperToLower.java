aace932dd5d17dc30d57a0a18253ff76e4d6bf65
kartikey <p.kartikey.p@gmail.com>
2025-07-31 10:18:24 +0530

    Kartikey Add UpperToLower

---
+++ JavaStreams/UpperToLower.java
@@ -0,0 +1,20 @@
+package JavaStreams;
+
+import java.io.*;
+
+public class UpperToLower {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+             BufferedWriter writer = new BufferedWriter(new FileWriter("output_lower.txt"))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line.toLowerCase());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
