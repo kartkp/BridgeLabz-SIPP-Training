7df342d7005f66dcb94e479f2415a82eee415c28
kartikey <p.kartikey.p@gmail.com>
2025-08-06 11:12:41 +0530

    CheckedException

---
+++ JavaExceptionHandling/CheckedException.java
@@ -0,0 +1,16 @@
+package JavaExceptionHandling;
+
+import java.io.*;
+
+public class CheckedException {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("data.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
+}
