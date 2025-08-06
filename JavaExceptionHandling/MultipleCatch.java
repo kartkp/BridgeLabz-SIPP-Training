7b0def691bf3e6e4da9c53a5f5733a7a8f33561c
kartikey <p.kartikey.p@gmail.com>
2025-08-06 11:16:38 +0530

    MultipleCatch

---
+++ JavaExceptionHandling/MultipleCatch.java
@@ -0,0 +1,21 @@
+package JavaExceptionHandling;
+
+import java.util.*;
+
+public class MultipleCatch {
+    public static void main(String[] args) {
+        int[] arr = null;
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            arr = new int[]{10, 20, 30, 40};
+            System.out.print("Enter index: ");
+            int index = sc.nextInt();
+            System.out.println("Value at index " + index + ": " + arr[index]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index!");
+        } catch (NullPointerException e) {
+            System.out.println("Array is not initialized!");
+        }
+    }
+}
