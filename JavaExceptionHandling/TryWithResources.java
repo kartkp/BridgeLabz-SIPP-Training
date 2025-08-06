60fe621ba026d0efb0e0ae060def96806691d503
kartikey <p.kartikey.p@gmail.com>
2025-08-06 11:19:01 +0530

    Kartikey Add TryWithResources

--- JavaExceptionHandling/TryWithResources.java
+++ JavaExceptionHandling/TryWithResources.java
@@ -1,7 +1,6 @@
 package JavaExceptionHandling;
 
 import java.io.*;
-
 public class TryWithResources {
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new FileReader("info.txt"))) {
